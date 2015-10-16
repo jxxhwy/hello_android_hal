02194e2e0bb1e022cf44179bcf4808bd4c1ae7ef
jxxhwy <jxxhwy88@gmail.com>
2015-10-16 17:21:22 +0800

    Android驱动开发全过程示例－基于android5.1.1

---
+++ hardware/libhardware/modules/hello/Android.mk
@@ -0,0 +1,10 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_MODULE_TAGS := optional
+LOCAL_PRELINK_MODULE := false
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_SHARED_LIBRARIES := liblog
+LOCAL_SRC_FILES := hello.c
+#LOCAL_SRC_FILES := $(call all-subdir-c-files)
+LOCAL_MODULE := hello.default
+include $(BUILD_SHARED_LIBRARY)
